b099432a339260f3c3adddc798917728eeb75569
Vedantjais04 <ved.jais.ant@gmail.com>
2026-02-08 11:53:02 +0530

    Time: 20 ms (10.18%), Space: 46.7 MB (22.95%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,39 +1,25 @@
 class Solution {
 
-    static HashMap<Character, Integer> makeFreqMap(String str) {
-        HashMap<Character, Integer> mp = new HashMap<>();
-
-        for (int i = 0; i < str.length(); i++) {
-            Character ch = str.charAt(i);
-
-            if (!mp.containsKey(ch)) {
-                mp.put(ch, 1);
-            } else {
-                int currFreq = mp.get(ch);
-                mp.put(ch, currFreq + 1);
-            }
-        }
-
-        return mp;
-    }
-
     public boolean isAnagram(String s, String t) {
         if (s.length() != t.length()) return false;
 
-        HashMap<Character, Integer> mp = makeFreqMap(s);
-        for(int i=0;i<t.length();i++)
-        {
-            Character ch=t.charAt(i);
-            if(!mp.containsKey(ch)) return false;
-            int currFreq = mp.get(ch);
-            mp.put(ch,currFreq-1);
+        HashMap<Character, Integer> mp = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (!mp.containsKey(ch)) 
+                mp.put(ch, 1);
+            else 
+                mp.put(ch, mp.get(ch) + 1);
         }
 
-        for(Integer i : mp.values()) {
-            if(i != 0) return false;
+        for (int i = 0; i < t.length(); i++) {
+            char ch = t.charAt(i);
+            if (!mp.containsKey(ch) || mp.get(ch) == 0) 
+                return false;
+            mp.put(ch, mp.get(ch) - 1);
         }
+
         return true;
-
-        
     }
 }
