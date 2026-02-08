6a76164ac72e66a027b3babacf068a51fdd65ea3
Vedantjais04 <ved.jais.ant@gmail.com>
2026-02-08 12:23:44 +0530

    Time: 3 ms (90.89%), Space: 46.4 MB (40.51%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,18 +1,24 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) return false;
-
-        int[] freq = new int[26];
-
-        for (int i = 0; i < s.length(); i++) {
-            freq[s.charAt(i) - 'a']++;
-            freq[t.charAt(i) - 'a']--;
+        if(s.length()!=t.length()){
+            return false;
         }
+        
+        char[] ch1=s.toCharArray();
+        char[] ch2=t.toCharArray();
+        Arrays.sort(ch1);
+        Arrays.sort(ch2);
 
-        for (int count : freq) {
-            if (count != 0) return false;
-        }
+        //Take for example s="anagram", t="nagaram"
+        //Example: s -> ['a','n','a','g','r','a','m']
+        //Example: t -> ['n','a','g','a','r','a','m']
+        //Arrays after sorting
+        //ch1 ->[a,a,a,g,m,n,r]->Sorted Alphabetically
+        //ch2 ->[a,a,a,g,m,n,r]->Sorted Alphabetically
 
-        return true;
+
+        return Arrays.equals(ch1,ch2)==true? true: false;
+        //We use Arrays.equals() here  because it checks if the
+        // two have the same values inside
     }
 }
