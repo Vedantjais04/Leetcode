7708a33775831b156d2fbf8d7d91c143724826f9
Vedant Jaiswal <ved.jais.ant@gmail.com>
2026-03-11 03:25:44 +0530

    Time: 0 ms (100%), Space: 43.4 MB (70.57%) - LeetHub

---
+++ 0274-h-index/0274-h-index.java
@@ -0,0 +1,16 @@
+class Solution {
+  public int hIndex(int[] citations) {
+    final int n = citations.length;
+    int accumulate = 0;
+    int[] count = new int[n + 1];
+
+    for (final int citation : citations)
+      ++count[Math.min(citation, n)];
+    for (int i = n; i >= 0; --i) {
+      accumulate += count[i];
+      if (accumulate >= i)
+        return i;
+    }
+    throw new IllegalArgumentException();
+  }
+}
